1e06d706344f0a4729d02df5362d77bf88ff5591
Sergey Klevtsov <s.klevzoff@gmail.com>
2023-07-26 02:08:07 -0700

    Add an explicit target for updating doxygen geos config, disable auto updating. Cleanup some version stuff.

--- src/coreComponents/mainInterface/initialization.cpp
+++ src/coreComponents/mainInterface/initialization.cpp
@@ -266,9 +266,6 @@ std::unique_ptr< CommandLineOptions > parseCommandLineOptions( int argc, char * 
 std::unique_ptr< CommandLineOptions > basicSetup( int argc, char * argv[], bool const parseCommandLine )
 {
   setupEnvironment( argc, argv );
-
-  outputVersionInfo();
-
   setupLAI();
 
   if( parseCommandLine )
